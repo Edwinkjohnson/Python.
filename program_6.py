853e7aeb2621ffd2854498ee82fe20d41faffb44
Edwinkjohnson <johnsonedwin051@gmail.com>
2025-07-17 14:35:59 +0530

    Delete program_6.py

--- program_6.py
+++
@@ -1,8 +0,0 @@
-# write a program that uses built-in functions to find the largest and smallest among three numbers
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-num3 = float(input("Enter the third number: "))
-largest = max(num1, num2, num3)
-smallest = min(num1, num2, num3)
-print("The largest number is:", largest)
-print("The smallest number is:", smallest)
